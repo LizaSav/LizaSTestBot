a0f34fa1339027c9e98b76ddc3204b32c7140ccc
Lizochka <lisalizalisa@mail.ru>
2022-02-27 12:27:54 +0300

    change bot class for try deploy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,17 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-public class HelloWorld {
+public class Main {
     @SneakyThrows
     public static void main(String[] args) throws TelegramApiException {
-        BotScript botScript = SheetsQuickstart.getScriptFromGoogleTables();
+        //      BotScript botScript = SheetsQuickstart.getScriptFromGoogleTables();
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         // Register our bot
         try {
             telegramBotsApi.registerBot(
-                    new HubBot(botScript)
-            //                                   new EchoBot()
+     //               new HubBot(botScript)
+                                               new EchoBot()
             );
         } catch (TelegramApiException e) {
             e.printStackTrace();
